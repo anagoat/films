ddc6982620531ee9f17f6f3aa1fd93f59ab51636
Anagoat <dp131192pka@gmail.com>
2020-01-23 08:54:15 +0200

    '0.3.1'

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -8,11 +8,11 @@ const Toolbar = () => {
     return (
         <div className="Toolbar">
             <div className='dropdown'>
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Котегории</button>
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Категории</button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <p className="dropdown-item" href="#"><Link to={'/Ужосы'}>Ужосы</Link></p>
+                    <p className="dropdown-item" href="#"><Link to={'/Ужасы'}>Ужосы</Link></p>
                     <p className="dropdown-item" href="#"><Link to={'/Триллеры'}>Триллеры</Link></p>
-                    <p className="dropdown-item" href="#"><Link to={'/Дромы'}>Дромы</Link></p>
+                    <p className="dropdown-item" href="#"><Link to={'/Драмы'}>Дромы</Link></p>
                 </div>
             </div>
             <button type='button' className="btn btn-secondary"> 
